0d156837e6879c86d3a02514add94f54e5ed6d9b
julienbrg <julien@strat.cc>
2022-12-13 12:28:20 +0100

    update config

--- contracts/SSD.sol
+++ contracts/SSD.sol
@@ -7,27 +7,46 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
 /// @custom:security-contact julien@strat.cc
-contract SSD is Governor, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
-    constructor(IVotes _token)
+contract SSD is
+    Governor,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorVotesQuorumFraction
+{
+    constructor(
+        IVotes _token
+    )
         Governor("SSD")
         GovernorVotes(_token)
-        GovernorVotesQuorumFraction(20)
+        GovernorVotesQuorumFraction(20) // Quorum set to 20%
     {}
 
     function votingDelay() public pure override returns (uint256) {
-        return 10; // 10 block
+        return 1; // Vote opens 1 block after proposal submission
     }
 
     function votingPeriod() public pure override returns (uint256) {
-        return 100800; // 2 week
+        return 240; // 240 * 15 / 60 ≈ 1 hour
     }
 
-    function quorum(uint256 blockNumber)
+    function quorum(
+        uint256 blockNumber
+    )
         public
         view
         override(IGovernor, GovernorVotesQuorumFraction)
         returns (uint256)
     {
         return super.quorum(blockNumber);
+    }
+
+    // Test
+    function addMember(address newMember) public {
+        // mint
+    }
+
+    // Test
+    function banMember(address newMember) public {
+        // burn
     }
 }
