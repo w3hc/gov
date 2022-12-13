d93fa0184e44e47e1c1ec8c0356c4cb3e3cfb3b0
julienbrg <julien@strat.cc>
2022-12-13 09:24:41 +0100

    rename test file

---
+++ test/SSD.ts
@@ -0,0 +1,44 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Signed Sealed Delivered", function () {
+
+  async function deployContracts() {
+    
+    const [deployer, alice, bob] = await ethers.getSigners();
+
+    const uri = "https://ipfs.io/ipfs/bafybeiberpia3qev7lvnusiiheqqfe57sk5r23gs6fh7v3v6vdcw6wrldq/metadata.json";
+    const Sugar = await ethers.getContractFactory("Sugar");
+    const sugar = await Sugar.deploy(alice.address, bob.address, uri);
+
+    const SSD = await ethers.getContractFactory("SSD");
+    const ssd = await SSD.deploy(sugar.address);
+
+    return { ssd, sugar, deployer, alice, bob };
+  }
+
+  describe("Deployment", function () {
+
+    it("Should own the right token IDs", async function () {
+
+      const { sugar, alice, bob } = await loadFixture(deployContracts);
+      expect(await sugar.ownerOf(0)).to.equal(alice.address);
+      expect(await sugar.ownerOf(1)).to.equal(bob.address);
+
+    });
+
+    it("Should set the right token address", async function () {
+
+      const { ssd, sugar } = await loadFixture(deployContracts);
+      expect(await ssd.token()).to.equal(sugar.address);
+
+    }); 
+  });
+  describe("Interactions", function () {
+
+    
+    
+  });
+});
