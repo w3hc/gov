d3905893269b0be4a6f5ba07e2b7feadf61bcfcf
julienbrg <julien@strat.cc>
2022-12-13 10:19:17 +0100

    add ownership transfer

--- test/SSD.ts
+++ test/SSD.ts
@@ -16,29 +16,36 @@ describe("Signed Sealed Delivered", function () {
     const SSD = await ethers.getContractFactory("SSD");
     const ssd = await SSD.deploy(sugar.address);
 
+    await sugar.transferOwnership(ssd.address);
+
     return { ssd, sugar, deployer, alice, bob };
   }
 
   describe("Deployment", function () {
 
     it("Should own the right token IDs", async function () {
-
       const { sugar, alice, bob } = await loadFixture(deployContracts);
       expect(await sugar.ownerOf(0)).to.equal(alice.address);
       expect(await sugar.ownerOf(1)).to.equal(bob.address);
-
     });
 
     it("Should set the right token address", async function () {
-
       const { ssd, sugar } = await loadFixture(deployContracts);
       expect(await ssd.token()).to.equal(sugar.address);
-
     }); 
-  });
-  describe("Interactions", function () {
 
+    it("Should transfer the NFT contract ownership to the Governor contract", async function () {
+      const { ssd, sugar } = await loadFixture(deployContracts);
+      expect(await sugar.owner()).to.equal(ssd.address);
+    }); 
+
+  });
+
+  describe("Interactions", function () {
     
-    
+    // TODO: Gov can mint NFTs
+    // TODO: Gov can burn NFTs
+    // TODO: Members can vote
+
   });
 });
