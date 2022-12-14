8c5fc85d6f54f436be547e2575e783afb87cbe87
julienbrg <julien@strat.cc>
2022-12-14 16:52:54 +0100

    add utils

---
+++ utils/move-blocks.ts
@@ -0,0 +1,11 @@
+import { network } from "hardhat"
+
+export async function moveBlocks(amount: number) {
+  for (let index = 0; index < amount; index++) {
+    await network.provider.request({
+      method: "evm_mine",
+      params: [],
+    })
+  }
+  console.log(`Moved ${amount} blocks`)
+}
