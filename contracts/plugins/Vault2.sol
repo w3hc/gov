83d46f113410daaae20fa4a9f49adc1751d885e5
julienbrg <julien@strat.cc>
2023-04-19 12:03:25 +0200

    add snapshot

--- contracts/plugins/Vault2.sol
+++ contracts/plugins/Vault2.sol
@@ -3,24 +3,38 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+contract Vault2 is ERC4626, Ownable, ERC20Snapshot {
+    uint256 public latestSnapshot;
 
-// contract Vault2 is ERC4626, ERC20Snapshot, Ownable {
-contract Vault2 is ERC4626, Ownable {
-    constructor(IERC20 asset_) ERC20("xUSDC", "xUSDC") ERC4626(asset_) {}
+    constructor(
+        IERC20 asset_
+    ) ERC20Snapshot() ERC20("xUSDC", "xUSDC") ERC4626(asset_) {}
 
     function govTransfer(uint256 amount) public onlyOwner {
         ERC20(asset()).transfer(msg.sender, amount);
     }
 
-    // function _beforeTokenTransfer(
-    //     address from,
-    //     address to,
-    //     uint256 amount
-    // ) internal override(ERC20, ERC20Snapshot) {
-    //     super._beforeTokenTransfer(from, to, amount);
+    // function snapshot(address account) public returns (uint256) {
+    //     return balanceOfAt(account, _snapshot());
     // }
+
+    function snapshot() public {
+        latestSnapshot = _snapshot();
+    }
+
+    function decimals() public pure override(ERC20, ERC4626) returns (uint8) {
+        return 18;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
     receive() external payable {}
 
