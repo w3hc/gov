5a725a0889b732bb58c8937509dd0263e4bea19d
julienbrg <julien@strat.cc>
2024-02-24 13:50:14 +0100

    add dao config file

---
+++ dao.config.ts
@@ -0,0 +1,21 @@
+///// Membership NFT /////
+
+export const firstMembers = [
+    "0xD8a394e7d7894bDF2C57139fF17e5CBAa29Dd977", // Alice
+    "0xe61A1a5278290B6520f0CEf3F2c71Ba70CF5cf4C" // Bob
+]
+
+export const uri =
+    "https://bafkreicj62l5xu6pk2xx7x7n6b7rpunxb4ehlh7fevyjapid3556smuz4y.ipfs.w3s.link/"
+export const name = "Membership NFT"
+export const symbol = "MEMBER"
+
+///// Gov /////
+
+export const manifesto =
+    "https://bafkreifnnreoxxgkhty7v2w3qwiie6cfxpv3vcco2xldekfvbiem3nm6dm.ipfs.w3s.link/"
+export const daoName = "Test DAO"
+export const votingDelay = 1
+export const votingPeriod = 300
+export const votingThreshold = 1
+export const quorum = 20
